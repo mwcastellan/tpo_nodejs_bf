e60c18738028f27442a1cfc5f99389bf6c7eac9f
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-09 18:26:03 -0300

    Marcelo W. Castellan

--- js/pedidos.js
+++ js/pedidos.js
@@ -73,7 +73,7 @@ function edit(id) {
   if (!pedido) return;
 
   document.getElementById("txtid").value = pedido.ID;
-  document.getElementById("txtfecha_compra").value = date(pedido.FECHA_COMPRA,"yyyy-mm-dd");
+  document.getElementById("txtfecha_compra").value = date(pedido.FECHA_COMPRA,"dd/mm/yyyy");
 
   document.getElementById("txtidcliente").value = pedido.IDCLIENTE;
 
