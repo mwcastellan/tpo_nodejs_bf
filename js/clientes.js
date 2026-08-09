8741e9d886bbf0f60177a2bd0d5e730099fe6004
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-09 00:35:48 -0300

    Marcelo W. Castellan

--- js/clientes.js
+++ js/clientes.js
@@ -1,42 +1,21 @@
-const URL_API = "https://tpodotnetbbapi-production.up.railway.app/api/clientes/registrar";
+const URL_API =
+  "https://tpodotnetbbapi-production.up.railway.app/api/clientes/registrar";
 // ALTA CLIENTES
 async function save() {
-  var data = {
-    EMAIL: document.getElementById("EMAIL").value,
-    APELLIDO: document.getElementById("APELLIDO").value,
-    NOMBRE: document.getElementById("NOMBRE").value,
-    DIRECCION: document.getElementById("DIRECCION").value,
-    PASSWORD: document.getElementById("PASSWORD").value,
-  };
-  var url = URL_API;
-  axios
-    .post(url, data, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-    .then((res) => {
-      console.log("then");
-      const ress = res.data.message;
-      let mensajesdeRes = "<ul>";
-      ress.forEach((ressi) => (mensajesdeRes += "<li>" + ressi.msg + "</li>"));
-      mensajesdeRes += "</ul>";
-      document.querySelector("#txtmsg").innerHTML = mensajesdeRes;
-    })
-    .catch((error) => {
-      console.log("catch");
-      if (error.response && error.response.status === 422) {
-        console.log(error.response.data.message);
-        const errores = error.response.data.message;
-        let mensajesdeError = "<ul>";
-        errores.forEach(
-          (error) => (mensajesdeError += "<li>" + error.msg + "</li>")
-        );
-        mensajesdeError += "</ul>";
-        document.querySelector("#txtmsg").innerHTML = mensajesdeError;
-      } else {
-        console.error(`Error en la solicitud "${error.message}`);
-      }
-    });
+  try {
+    const data = {
+      EMAIL: document.getElementById("EMAIL").value,
+      APELLIDO: document.getElementById("APELLIDO").value,
+      NOMBRE: document.getElementById("NOMBRE").value,
+      DIRECCION: document.getElementById("DIRECCION").value,
+      PASSWORD: document.getElementById("PASSWORD").value,
+    };
+
+    const res = await axios.post(URL_API, data);
+
+    document.querySelector("#txtmsg").innerHTML = res.data.mensaje;
+  } catch (error) {
+    document.querySelector("#txtmsg").innerHTML =
+      error.response?.data?.mensaje || error.message;
+  }
 }
