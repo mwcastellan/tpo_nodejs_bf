605773bb1a5f185ca1def9f8b9dc51f13dd892d7
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-09 01:38:51 -0300

    Marcelo W. Castellan

--- js/clientes.js
+++ js/clientes.js
@@ -1,7 +1,9 @@
-const URL_API =
-  "https://tpodotnetbbapi-production.up.railway.app/api/clientes/registrar";
+const URL_API = "https://tpodotnetbbapi-production.up.railway.app/api/clientes/registrar";
+
 // ALTA CLIENTES
 async function save() {
+  const txtMsg = document.querySelector("#txtmsg");
+
   try {
     const data = {
       EMAIL: document.getElementById("EMAIL").value,
@@ -13,9 +15,33 @@ async function save() {
 
     const res = await axios.post(URL_API, data);
 
-    document.querySelector("#txtmsg").innerHTML = res.data.mensaje;
+    txtMsg.innerHTML = res.data.mensaje;
+
   } catch (error) {
-    document.querySelector("#txtmsg").innerHTML =
-      error.response?.data?.mensaje || error.message;
+
+    // Error de validación ASP.NET
+    if (error.response?.data?.errors) {
+
+      const errores = error.response.data.errors;
+
+      let html = "<ul>";
+
+      Object.keys(errores).forEach(campo => {
+        errores[campo].forEach(mensaje => {
+          html += `<li>${mensaje}</li>`;
+        });
+      });
+
+      html += "</ul>";
+
+      txtMsg.innerHTML = html;
+
+    } else {
+
+      // Error simple (ej: email ya existe)
+      txtMsg.innerHTML =
+        error.response?.data?.mensaje ||
+        error.message;
+    }
   }
 }
